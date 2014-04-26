76336f5b62631de2c32b122ce0d3fc63baeea94e
JustBeast <adityabhardwaj222@gmail.com>
2014-04-25 17:57:21 -0700

    pythonroom update

--- goldbach.py
+++ goldbach.py
@@ -9,17 +9,28 @@ def goldbach(x):
 	for i in range(2, x):
 		if isPrime(i) == True:
 			evaluate=x-i
+			#finds prime numbers and stores just uses them straight away.  it does not store it in a list.
 			if isPrime(evaluate) == True:
 				print str(i) + " + " + str(evaluate) + " = " + str(x)
 				return True
+			#subtracts prime numbers from initial number to see if diff == a prime number 
+
 	return False
+
 def inathousand():
 	for u in range(4, 1000):
 		if goldbach(u) == False:
 			print u
 			return "Found it"
-
-			
 def goldbach_triples(x):
+	for i in range(2, x):
+		if isPrime(i) == True:
+			evaluate=x-i
+			if goldbach(evaluate) == True:
+				print str(i)
+			return True
+goldbach_triples(3458)
+			
+
 	
 		
