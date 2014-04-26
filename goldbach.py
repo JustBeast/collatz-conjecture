ced52ed95e12ad7517a9025b0bf4945833433d1d
JustBeast <adityabhardwaj222@gmail.com>
2014-04-25 17:08:40 -0700

    pythonroom update

--- goldbach.py
+++ goldbach.py
@@ -1,3 +1,13 @@
 # author: JustBeast
+def isPrime(n):
+	for x in range (2, n/2 + 1): #it is n/2 + 1 because of the range function
+	if n%x == 0:
+			return False
+	return True
+prime=[]
 def goldbach(x):
-	for x in range(1, x)
+	for x in range(2, x):
+		if isPrime(x) == True:
+			prime.append(x)
+for 
+		
